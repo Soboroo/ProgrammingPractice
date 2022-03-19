4b9fd576568cdd0210eda832aba777a149bf653c
Soboroo <woo036325@gmail.com>
2022-03-19 14:13:40 +0900

    .

---
+++ golang/10820.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		fmt.Println(text)
+	}
+}
