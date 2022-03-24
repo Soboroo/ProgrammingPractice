dea8e5a414ae0d8bfc4a0c8396e7483638c63746
Soboroo <woo036325@gmail.com>
2022-03-24 16:50:15 +0900

    .

--- hellocpp/other/12865.cpp
+++ hellocpp/other/12865.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
-#include <map>
-#include <stack>
+#include <vector>
 using namespace std;
-map<int, int> goondaerani;
-stack<map<int, int>> s;
-int n, k;
-int knapsack(int index, int size) {
-  if (size > k)
-    return size;
-  else {
-  }
-}
 
 int main() {
-
+  cin.tie(0);
+  cout.tie(0);
+  ios::sync_with_stdio(false);
+  int n, k;
   cin >> n >> k;
+  vector<pair<int, int>> v(n);
+  for (int i = 0; i < n; i++)
+    cin >> v[i].first >> v[i].second;
+  vector<vector<int>> dp(n + 1, vector<int>(k + 1));
+  for (int i = 0; i <= n; i++)
+    for (int j = 0; j <= k; j++)
+      if (i == 0 || j == 0)
+        dp[i][j] = 0;
+      else if (v[i - 1].first > j)
+        dp[i][j] = dp[i - 1][j];
+      else
+        dp[i][j] =
+            max(dp[i - 1][j], dp[i - 1][j - v[i - 1].first] + v[i - 1].second);
 
-  for (int i = 0; i < n; i++) {
-    int x, y;
-    cin >> x >> y;
-    goondaerani.insert(make_pair(x, y));
-  }
+  cout << dp[n][k];
 }
