bbf25ca5cc14b3bf7fef4c7f7720099e26965830
Soboroo <woo036325@gmail.com>
2022-04-29 22:09:40 +0900

    .

--- hellocpp/other/11505.cpp
+++ hellocpp/other/11505.cpp
@@ -3,57 +3,62 @@
 #define MOD 1000000007
 using namespace std;
 
-void init(vector<int> &a, vector<int> &tree, int n, int l, int r) {
+void init(vector<int> &a, vector<int> &tree, int node, int l, int r) {
   if (l == r) {
-    tree[n] = a[l];
-  } else {
-    int mid = (l + r) / 2;
-    init(a, tree, n * 2, l, mid);
-    init(a, tree, n * 2 + 1, mid + 1, r);
-    tree[n] = tree[n * 2] * tree[n * 2 + 1] % MOD;
-  }
-}
-
-void update(vector<int> &tree, int n, int l, int r, int i, int val) {
-  if (l == r) {
-    tree[n] = val;
-  } else {
-    int mid = (l + r) / 2;
-    update(tree, n * 2, l, mid, i, val);
-    update(tree, n * 2 + 1, mid + 1, r, i, val);
-    tree[n] = tree[n * 2] * tree[n * 2 + 1] % MOD;
-  }
-}
-
-int query(vector<int> &tree, int n, int l, int r, int i, int j) {
-  if (l > j || r < i) {
-    return 1;
-  }
-  if (i <= l && r <= j) {
-    return tree[n];
+    tree[node] = a[l];
+    return;
   }
   int mid = (l + r) / 2;
-  return query(tree, n * 2, l, mid, i, j) *
-         query(tree, n * 2 + 1, mid + 1, r, i, j) % MOD;
+  init(a, tree, node * 2, l, mid);
+  init(a, tree, node * 2 + 1, mid + 1, r);
+  tree[node] = (tree[node * 2] % MOD) * (tree[node * 2 + 1] % MOD) % MOD;
+}
+
+int query(vector<int> &tree, int node, int l, int r, int i, int j) {
+  if (i > r || j < l)
+    return 1;
+  if (i <= l && r <= j)
+    return tree[node];
+  int mid = (l + r) / 2;
+  int left = query(tree, node * 2, l, mid, i, j);
+  int right = query(tree, node * 2 + 1, mid + 1, r, i, j);
+  return (left % MOD) * (right % MOD) % MOD;
+}
+
+void update(vector<int> &a, vector<int> &tree, int node, int l, int r,
+            int index, int value) {
+  if (index < l || index > r)
+    return;
+  if (l == r) {
+    tree[node] = value;
+    a[index] = value;
+    return;
+  }
+  int mid = (l + r) / 2;
+  update(a, tree, node * 2, l, mid, index, value);
+  update(a, tree, node * 2 + 1, mid + 1, r, index, value);
+  tree[node] = tree[node * 2] * tree[node * 2 + 1] % MOD;
 }
 
 int main() {
+  cin.tie(0);
+  ios_base::sync_with_stdio(false);
   int n, m, k;
   cin >> n >> m >> k;
-  vector<int> a(n);
   m += k;
-  for (int i = 0; i < n; i++) {
-    cin >> a[i];
-  }
+  vector<int> a(n);
   vector<int> tree(4 * n);
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
   init(a, tree, 1, 0, n - 1);
-  for (int i = 0; i < m; i++) {
-    int x, y, z;
-    cin >> x >> y >> z;
-    if (x == 1) {
-      update(tree, 1, 0, n - 1, y - 1, z);
+  while (m--) {
+    int i, j, k;
+    cin >> i >> j >> k;
+    if (i == 1) {
+      update(a, tree, 1, 0, n - 1, j - 1, k);
     } else {
-      cout << query(tree, 1, 0, n - 1, y - 1, z - 1) << endl;
+      int ans = query(tree, 1, 0, n - 1, j - 1, k - 1);
+      cout << ans << '\n';
     }
   }
 }
