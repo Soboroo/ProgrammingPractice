b2086ca2d888f3a85d63c4ba373c15145607de05
Yeongyun Woo <woo036325@gmail.com>
2022-11-15 14:28:15 +0000

    .

--- hellocpp/other/9663.cpp
+++ hellocpp/other/9663.cpp
@@ -2,38 +2,34 @@
 #include <vector>
 using namespace std;
 
-vector<int> chess;
-int answer = 0;
-bool check(int x, int y) {
-  if (chess.size() == 0)
-    return true;
-  if (x < 0 || x >= chess.size() || y < 0 || y >= chess.size())
-    return false;
-  if (chess[x] == 0)
-    return true;
-  if (chess[x] == chess[y])
-    return false;
+bool checkVaild(vector<int> &v, int n, int x, int y) {
+  for (int i = 0; i < x; i++) {
+    if (v[i] == y)
+      return false;
+    if (abs(v[i] - y) == abs(i - x))
+      return false;
+  }
   return true;
 }
 
-void backtrack(vector<int> &v, int n, int k) {
-  if (k == n) {
-    answer++;
-    return;
-  }
-  for (int i = 0; i < n; i++) {
-    if (check(k, i)) {
-      v[k] = i;
-      backtrack(v, n, k + 1);
-      v.pop_back();
+int backtrace(int n, int column, vector<int> &v) {
+  if (column == n) {
+    return 1;
+  } else {
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+      if (checkVaild(v, n, column, i)) {
+        v[column] = i;
+        sum += backtrace(n, column + 1, v);
+      }
     }
+    return sum;
   }
 }
-
 int main() {
   int n;
   cin >> n;
-  chess.resize(n);
-  backtrack(chess, n, 0);
+  vector<int> v(n, 0);
+  cout << backtrace(n, 0, v) << endl;
   return 0;
 }
